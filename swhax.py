ac222cd8b50d7680d0d0f4960616d2451e49a063
Corian (Cory) Kennedy <cory@darkintel.io>
2023-06-28 18:12:39 -0500

    Update swhax.py

--- swhax.py
+++ swhax.py
@@ -61,7 +61,7 @@ notification_handler = CustomNotificationHandler(flight_retriever)
 st.sidebar.markdown(
     """
     <div style="display: flex; align-items: center;">
-        <img src="https://simpleicons.org/icons/southwestairlines.svg" alt="Logo" width="30">
+        <img src="data:image/svg+xml,%3Csvg fill='%23304CB2' role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3ESouthwest Airlines%3C/title%3E%3Cpath d='M22.163 2.419C21.038 1.219 19.35.58 17.437.58c-2.062 0-3.637.675-4.725 1.275 2.063 1.163 6.526 3.75 11.175 7.163.075-.45.113-.938.113-1.388-.038-2.175-.675-4.012-1.837-5.212zm1.35 8.212C18.186 6.244 15 4.031 11.55 1.97 10.612 1.406 8.775.58 6.675.58 4.688.581 3 1.22 1.837 2.42 1.087 3.206.563 4.18.262 5.38 3 7.294 10.462 12.656 18 18.581c2.512-2.362 4.613-5.1 5.512-7.95zM0 7.781c0 6.15 6.487 11.85 12 15.638 1.575-1.088 3.225-2.325 4.8-3.713A736.871 736.871 0 0 0 .15 6.131C.038 6.62 0 7.181 0 7.781Z'/%3E%3C/svg%3E" alt="Logo" width="30">
         <h2 style="margin-left: 10px;">Southwest Checker Inner</h2>
     </div>
     """,
@@ -114,13 +114,6 @@ if st.sidebar.button('Run Checkin'):
 if st.button('Refresh Log'):
     display_log_content()
 
-# Real-time log search
-if search_realtime:
-    search_log_realtime()
-
-# Display log file content if the checkbox is checked
-if show_log:
-    display_log_content()
 # Real-time log search
 if search_realtime:
     search_log_realtime()
