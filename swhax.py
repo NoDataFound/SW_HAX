0a1b184fc7dce035aa21f39d8a1e5f32c4514306
Corian (Cory) Kennedy <cory@darkintel.io>
2023-06-28 17:26:06 -0500

    Update swhax.py

--- swhax.py
+++ swhax.py
@@ -4,9 +4,22 @@ from lib.main import main
 from lib.notification_handler import CustomNotificationHandler
 from lib.flight_retriever import FlightRetriever
 from lib.config import Config
+import time
 
+# Function to read and display the log file content
+def display_log_content():
+    try:
+        with open(log_file_path, 'r') as file:
+            log_content = file.read()
+
+        # Display the content of the log file
+        st.markdown('**Log File Content:**')
+        st.code(log_content)
+
+    except Exception as e:
+        st.write(f'An error occurred while reading the log file: {str(e)}')
 # Create an instance of NotificationHandler and pass the flight_retriever instance to it
-log_file_path = "logs/auto-southwest-check-in.log"
+log_file_path = "/Users/0xdeadbeef/Repositories/SW_HAX/logs/auto-southwest-check-in.log"
 config =Config()
 flight_retriever = FlightRetriever(config)
 notification_handler = CustomNotificationHandler(flight_retriever)
@@ -30,11 +43,12 @@ st.markdown('----')
 confirmation_number = st.sidebar.text_input('Confirmation Number')
 first_name = st.sidebar.text_input('First Name')
 last_name = st.sidebar.text_input('Last Name')
-
+# Checkbox to show log
+show_log = st.sidebar.checkbox('Show Log')
 # Button to start the script
 if st.sidebar.button('Run Checkin'):
     if confirmation_number and first_name and last_name:
-        st.markdown('Running the checkin process...')
+        st.markdown('Running the check-in process...')
         arguments = [confirmation_number, first_name, last_name, "--verbose"]
         st.write(arguments)
         try:
@@ -49,17 +63,7 @@ if st.sidebar.button('Run Checkin'):
             for phrase in success_phrases:
                 if phrase in output:
                     st.markdown(f"**{phrase}**")
-            
-            try:
-                with open(log_file_path, 'r') as file:
-                    log_content = file.read()
 
-                # Display the content of the log file
-                st.markdown('**Log File Content:**')
-                st.code(log_content)
-
-            except Exception as e:
-                st.write(f'An error occurred while reading the log file: {str(e)}')
         except Exception as e:
             st.write(f'An error occurred: {str(e)}')
 
@@ -67,15 +71,13 @@ if st.sidebar.button('Run Checkin'):
         st.sidebar.warning('Please input all required fields')
 else:
     st.markdown('Waiting for user input...')
+
 # Button to refresh the log file content
 if st.button('Refresh Log'):
-    try:
-        with open(log_file_path, 'r') as file:
-            log_content = file.read()
+    display_log_content()
 
-        # Display the refreshed content of the log file
-        st.markdown('**Refreshed Log File Content:**')
-        st.code(log_content)
-
-    except Exception as e:
-        st.write(f'An error occurred while reading the log file: {str(e)}')
+# Display log content periodically
+if show_log:
+    while True:
+        display_log_content()
+        time.sleep(10)
